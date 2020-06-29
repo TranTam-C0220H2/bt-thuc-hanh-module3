ceed47800b0b22bbf73d477f9eb5caae65883949
TranTam-C0220H2 <tam.tvt.ptit@gmail.com>
2020-06-29 12:30:58 +0700

    edit

--- src/app/book/list-book/list-book.component.ts
+++ src/app/book/list-book/list-book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../book.service';
+import { SearchService } from 'src/app/search.service';
 
 @Component({
   selector: 'app-list-book',
@@ -8,12 +9,16 @@ import { BookService } from '../book.service';
 })
 export class ListBookComponent implements OnInit {
   listBook;
-  constructor(private booService: BookService) { }
+  constructor(private booService: BookService, private searchService: SearchService) { }
 
   ngOnInit(): void {
     this.booService.index().subscribe(result => {
       this.listBook = result;
-    })
+    });
+    this.searchService.searchInput.subscribe( keywords => {
+      this.listBook = this.listBook
+        .filter( item => item.title.toLowerCase().includes(keywords.toLowerCase()) );
+    });
   }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
+  }
+
+  search(event) {
+    const keywords = event.target.value;
+    this.searchService.searchInput.next(keywords);
   }
 
 }
